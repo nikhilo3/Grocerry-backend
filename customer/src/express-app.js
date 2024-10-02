c2b2346a0497fb4bfda4542994c9213520746017
nikhilo3 <nikhilladani03@gmail.com>
2024-10-02 22:27:43 +0530

    some

--- customer/src/express-app.js
+++ customer/src/express-app.js
@@ -8,10 +8,7 @@ module.exports = async (app) => {
 
     app.use(express.json({ limit: '1mb'}));
     app.use(express.urlencoded({ extended: true, limit: '1mb'}));
-    app.use(cors({
-        origin: 'https://grocerry-admin.vercel.app', // Replace with your frontend URL
-        credentials: true
-      }));
+    app.use(cors());
     app.use(express.static(__dirname + '/public'))
 
     //Listen to events//
