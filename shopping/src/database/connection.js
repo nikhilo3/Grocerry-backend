7606c75a91901a7e5256d24a5d6b9d09a899b89c
nikhilo3 <nikhilladani03@gmail.com>
2024-10-03 00:20:52 +0530

    changes

--- shopping/src/database/connection.js
+++ shopping/src/database/connection.js
@@ -4,7 +4,7 @@ const { DB_URL } = require('../config');
 module.exports = async() => {
 
     try {
-        await mongoose.connect(DB_URL, {
+        await mongoose.connect('mongodb+srv://nikhilladani03:MdDnIyrZT2NSYF5f@cluster0.vzpap.mongodb.net/inventorymanagement', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
