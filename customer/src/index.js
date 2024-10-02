f3449884b61b85e20538de8b88ff80823815e544
nikhilo3 <nikhilladani03@gmail.com>
2024-10-02 22:53:40 +0530

    index

--- customer/src/index.js
+++ customer/src/index.js
@@ -1,26 +1,62 @@
+// const express = require('express');
+// const { PORT } = require('./config');
+// const { databaseConnection } = require('./database');
+// const expressApp = require('./express-app');
+// // const { CreateChannel } = require('./utils');
+
+// const StartServer = async() => {
+
+//     const app = express();
+    
+//     await databaseConnection();
+    
+//     // const channel = await CreateChannel();
+
+//     await expressApp(app);
+
+//     app.listen(PORT, () => {
+//         console.log(`listening to port ${PORT}`);
+//     })
+//     .on('error', (err) => {
+//         console.log(err);
+//         process.exit();
+//     })
+// }
+
+// StartServer();
+
+
+
+
+
+
+
+
+
 const express = require('express');
-const { PORT } = require('./config');
+const { PORT } = require('./config'); // This is usually ignored in serverless
 const { databaseConnection } = require('./database');
 const expressApp = require('./express-app');
-// const { CreateChannel } = require('./utils');
 
-const StartServer = async() => {
+const app = express();
 
-    const app = express();
-    
+const StartServer = async () => {
     await databaseConnection();
-    
-    // const channel = await CreateChannel();
-
     await expressApp(app);
+};
 
+// Call StartServer only for local development
+if (require.main === module) {
+    StartServer();
+    
+    // For local testing
     app.listen(PORT, () => {
-        console.log(`listening to port ${PORT}`);
-    })
-    .on('error', (err) => {
+        console.log(`Listening to port ${PORT}`);
+    }).on('error', (err) => {
         console.log(err);
         process.exit();
-    })
+    });
 }
 
-StartServer();
+// Export the app for Vercel
+module.exports = app;
